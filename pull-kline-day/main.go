49c4f4e35e2355e5edf43f60268d50a9a0bd0e6b
injoyai <1113655791@qq.com>
2025-04-09 16:19:01 +0800

    101

--- pull-kline-day/main.go
+++ pull-kline-day/main.go
@@ -19,6 +19,11 @@ func main() {
 	m, err := tdx.NewManage(nil, tdx.WithRedial())
 	logs.PanicErr(err)
 
+	//{
+	//	do2(m, m.Codes.GetStocks(), time.Now().AddDate(0, 0, -800), time.Now())
+	//	return
+	//}
+
 	err = do(m, m.Codes.GetStocks())
 	logs.PrintErr(err)
 
@@ -81,4 +86,74 @@ func do(m *tdx.Manage, codes []string) error {
 		return err
 	}
 	return oss.New("./data/增量/日线/"+time.Now().Format("2006/2006-01-02")+".csv", buf)
+}
+
+func do2(m *tdx.Manage, codes []string, start, end time.Time) error {
+
+	all := make(map[string][]*Kline)
+
+	for _, code := range codes {
+		logs.Debug("开始执行:", code)
+		err := m.Do(func(c *tdx.Client) error {
+			resp, err := c.GetKlineDay(code, 0, 800)
+			if err != nil {
+				return err
+			}
+			for _, k := range resp.List {
+				all[k.Time.Format(time.DateOnly)] = append(all[k.Time.Format(time.DateOnly)], &Kline{
+					Code:  code,
+					Name:  m.Codes.GetName(code),
+					Kline: k,
+				})
+			}
+			return nil
+		})
+		logs.PrintErr(err)
+	}
+
+	for i := start; i.Before(end); i = i.AddDate(0, 0, 1) {
+		if !m.Workday.Is(i) {
+			logs.Info(i.Format("2006-01-02") + "不是工作日")
+			continue
+		}
+
+		data := [][]any{
+			{"序号", "代码", "名称", "日期", "昨收", "开盘", "收盘", "最高", "最低", "成交量", "成交额", "振幅", "涨跌幅"},
+		}
+
+		ls := all[i.Format(time.DateOnly)]
+
+		for ii, l := range ls {
+			data = append(data, []any{
+				ii + 1,
+				l.Code,
+				l.Name,
+				i.Format("2006-01-02"),
+				l.Last.Float64(),
+				l.Open.Float64(),
+				l.Close.Float64(),
+				l.High.Float64(),
+				l.Low.Float64(),
+				l.Volume,
+				l.Amount.Float64(),
+				l.RisePrice().Float64(),
+				l.RiseRate(),
+			})
+		}
+		buf, err := excel.ToCsv(data)
+		if err != nil {
+			logs.Err(err)
+			continue
+		}
+		oss.New("./data/增量/日线/"+i.Format("2006/2006-01-02")+".csv", buf)
+
+	}
+	return nil
+
+}
+
+type Kline struct {
+	Code string
+	Name string
+	*protocol.Kline
 }
