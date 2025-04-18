191a49d19119588a1579301c2adbf90cd778dc7a
injoyai <1113655791@qq.com>
2025-04-18 17:00:43 +0800

    101

--- pull-daykline-once/main.go
+++ pull-daykline-once/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/injoyai/goutil/g"
 	"github.com/injoyai/logs"
 	"time"
 )
@@ -14,43 +13,44 @@ func init() {
 func main() {
 	defer done()()
 
-	startDate := time.Date(2025, 2, 17, 0, 0, 0, 0, time.Local)
-	endDate := time.Date(2025, 3, 31, 23, 0, 0, 0, time.Local)
-	size := 200
+	startDate := time.Date(2021, 1, 1, 0, 0, 0, 0, time.Local)
+	endDate := time.Date(2025, 4, 16, 23, 0, 0, 0, time.Local)
 
-	{
-		var err error
-		for {
-			start := g.Input("请输入开始时间(例2025-02-17): ")
-			startDate, err = time.Parse("2006-01-02 15:04:05", start+" 00:00:00")
-			if err == nil {
-				break
-			}
-			logs.Err(err)
-		}
-		for {
-			end := g.InputVar("请输入结束时间(默认今天): ").String(time.Now().Format(time.DateOnly))
-			endDate, err = time.Parse("2006-01-02 15:04:05", end+" 23:00:00")
-			if err == nil {
-				break
-			}
-			logs.Err(err)
-		}
-		size = g.InputVar("请数据每个文件代码数量(默认6000):").Int(6000)
-		logs.Info("准备开始下载数据...")
-	}
+	//size := 200login
+	//
+	//{
+	//	var err error
+	//	for {
+	//		start := g.Input("请输入开始时间(例2025-02-17): ")
+	//		startDate, err = time.Parse("2006-01-02 15:04:05", start+" 00:00:00")
+	//		if err == nil {
+	//			break
+	//		}
+	//		logs.Err(err)
+	//	}
+	//	for {
+	//		end := g.InputVar("请输入结束时间(默认今天): ").String(time.Now().Format(time.DateOnly))
+	//		endDate, err = time.Parse("2006-01-02 15:04:05", end+" 23:00:00")
+	//		if err == nil {
+	//			break
+	//		}
+	//		logs.Err(err)
+	//	}
+	//	size = g.InputVar("请数据每个文件代码数量(默认6000):").Int(6000)
+	//	logs.Info("准备开始下载数据...")
+	//}
+	//
+	//bySector(
+	//	[]string{},
+	//	startDate,
+	//	endDate,
+	//	size,
+	//)
 
-	bySector(
+	byStock(
 		[]string{},
 		startDate,
 		endDate,
-		size,
 	)
-
-	//byStock(
-	//	[]string{},
-	//	time.Date(2016, 1, 1, 0, 0, 0, 0, time.Local),
-	//	time.Date(2025, 3, 23, 23, 0, 0, 0, time.Local),
-	//)
 
 }
