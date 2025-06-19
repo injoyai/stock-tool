790871f5be409f73824b618face5bb7319d8e010
injoyai <1113655791@qq.com>
2025-06-19 17:07:00 +0800

    优化

--- pull-trade/model.go
+++ pull-trade/model.go
@@ -6,9 +6,9 @@ type Trade struct {
 	ID       int64
 	Exchange string         //交易所
 	Code     string         `xorm:"index"` //代码
-	Show     string         //日期可视化
 	Date     uint16         `xorm:"index"` //日期
 	Time     uint16         `xorm:"index"` //时间
+	Show     string         //日期可视化,后续会删除
 	Price    protocol.Price //成交价格,单位厘
 	Volume   int            //交易量
 	Order    int            //订单数
