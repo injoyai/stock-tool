14e4bc30007700d14715cd641d315f7a7b04e800
injoyai <1113655791@qq.com>
2025-06-27 17:27:22 +0800

    优化

--- pull-trade/export.go
+++ pull-trade/export.go
@@ -1,80 +1,129 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"github.com/injoyai/base/types"
 	"github.com/injoyai/conv"
-	"github.com/injoyai/goutil/database/sqlite"
 	"github.com/injoyai/goutil/oss"
-	"github.com/injoyai/goutil/other/csv"
 	"github.com/injoyai/tdx"
-	"path/filepath"
 	"time"
 )
 
-func NewExport(codes []string, database, export string) *Export {
+func NewExport(codes []string, years []int, database, export string) *Export {
 	return &Export{
-		Database: dir(database),
-		Codes:    codes,
-		Export:   export,
+		Database:  dir(database),
+		Codes:     codes,
+		Years:     years,
+		Export:    export,
+		ReadLimit: 10,
 	}
 }
 
 type Export struct {
-	Database dir
-	Codes    []string
-	Export   string
+	Database  dir
+	Codes     types.List[string]
+	Years     []int
+	Export    string
+	ReadLimit int
+	HHD[any, any]
 }
 
-func (this *Export) Run(ctx context.Context, m *tdx.Manage) {
+func (this *Export) Run(ctx context.Context, m *tdx.Manage) error {
 	codes := this.Codes
 	if len(codes) == 0 {
 		codes = m.Codes.GetStocks()
 	}
-	for _, code := range this.Codes {
-		this.Database.rangeYear(code, func(year int, filename string, exist, hasNext bool) (bool, error) {
-			db, err := sqlite.NewXorm(filename)
-			if err != nil {
-				return false, err
-			}
-			defer db.Close()
-			start := time.Date(year, 1, 1, 0, 0, 0, 0, time.Local)
-			end := time.Date(year, 12, 31, 0, 0, 0, 0, time.Local).Add(1)
-			last := 0.
-			kss := []*Kline(nil)
-			for i := start; i.Before(end); i = i.Add(time.Hour * 24) {
-				if !m.Workday.Is(i) {
-					continue
-				}
-				date, _ := FromTime(i)
-				data := Trades{}
-				err = db.Where("Date=?", date).Find(&data)
-				if err != nil {
-					return false, err
-				}
-				ks := data.Kline1(date, last)
-				kss = append(kss, ks...)
-			}
-			xx := [][]any(nil)
-			for _, v := range kss {
-				xx = append(xx, []any{
-					v.Time.Format(time.DateTime),
-					v.Open,
-					v.High,
-					v.Low,
-					v.Close,
-					v.Volume,
-					v.Amount,
-				})
-			}
-			buf, err := csv.Export(xx)
-			if err != nil {
-				return false, err
-			}
-			oss.New(filepath.Join(this.Export, conv.String(year), "k线-1分钟", code+".csv"), buf)
-			return true, nil
-		})
-	}
 
+	//for _, year := range this.Years {
+	//	for _, cs := range codes.Split(this.ReadLimit) {
+	//		this.HHD.Run(ctx)
+	//	}
+	//}
+	return nil
+}
+
+func (this *Export) read(year int, codes []string) ([]any, error) {
+	//for _, v := range codes {
+	//
+	//}
+	return nil, nil
+}
+
+func (this *Export) deal() {
+	//db, err := sqlite.NewXorm(filename)
+	//if err != nil {
+	//	return err
+	//}
+	//defer db.Close()
+	//start := time.Date(year, 1, 1, 0, 0, 0, 0, time.Local)
+	//end := time.Date(year, 12, 31, 0, 0, 0, 0, time.Local).Add(1)
+	//last := 0.
+	//kss := []*Kline(nil)
+	//for i := start; i.Before(end); i = i.Add(time.Hour * 24) {
+	//	if !m.Workday.Is(i) {
+	//		continue
+	//	}
+	//	date, _ := FromTime(i)
+	//	data := Trades{}
+	//	err = db.Where("Date=?", date).Find(&data)
+	//	if err != nil {
+	//		return false, err
+	//	}
+	//	ks := data.Kline1(date, last)
+	//	kss = append(kss, ks...)
+	//}
+	//xx := [][]any(nil)
+	//for _, v := range kss {
+	//	xx = append(xx, []any{
+	//		v.Time.Format(time.DateTime),
+	//		v.Open,
+	//		v.High,
+	//		v.Low,
+	//		v.Close,
+	//		v.Volume,
+	//		v.Amount,
+	//	})
+	//}
+	//buf, err := csv.Export(xx)
+	//if err != nil {
+	//	return false, err
+	//}
+	//oss.New(filepath.Join(this.Export, conv.String(year), "k线-1分钟", code+".csv"), buf)
+	//return true, nil
+}
+
+func (this *Export) save(t *exportTask) (err error) {
+	err = oss.New(t.Filename, t.K1)
+	if err != nil {
+		return err
+	}
+	err = oss.New(t.Filename, t.K5)
+	if err != nil {
+		return err
+	}
+	err = oss.New(t.Filename, t.K15)
+	if err != nil {
+		return err
+	}
+	err = oss.New(t.Filename, t.K30)
+	if err != nil {
+		return err
+	}
+	err = oss.New(t.Filename, t.K60)
+	if err != nil {
+		return err
+	}
+	return
+}
+
+type exportTask struct {
+	Filename string
+	K1       *bytes.Buffer
+	K5       *bytes.Buffer
+	K15      *bytes.Buffer
+	K30      *bytes.Buffer
+	K60      *bytes.Buffer
 }
 
 /*
