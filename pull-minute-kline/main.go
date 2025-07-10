c70fa689054f5c40ffec6a545f3a4013b105260f
injoyai <1113655791@qq.com>
2025-07-10 09:10:54 +0800

    单独拎出来一个项目

---
+++ pull-minute-kline/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/injoyai/conv/cfg"
+	"github.com/injoyai/logs"
+	"github.com/injoyai/tdx"
+	"github.com/robfig/cron/v3"
+	"path/filepath"
+	"time"
+)
+
+var (
+	Clients     = cfg.GetInt("clients", 4)
+	Coroutines  = cfg.GetInt("coroutines", 10)
+	DatabaseDir = cfg.GetString("database", "./data/database")
+	ExportDir   = cfg.GetString("export", "./data/export")
+	Spec        = cfg.GetString("spec", "0 10 15 * * *")
+	Codes       = cfg.GetStrings("codes")
+	Startup     = cfg.GetBool("startup")
+)
+
+func init() {
+	logs.SetFormatter(logs.TimeFormatter)
+	logs.Info("版本:", "v0.2.7")
+	logs.Info("说明:", "单独拎出来一个项目")
+	logs.Info("任务规则:", Spec)
+	logs.Info("立马执行:", Startup)
+	logs.Info("连接数量:", Clients)
+	logs.Info("协程数量:", Coroutines)
+	fmt.Println("=====================================================")
+
+}
+
+func main() {
+	m, err := tdx.NewManage(&tdx.ManageConfig{Number: Clients})
+	logs.PanicErr(err)
+
+	f := func() {
+		logs.Info("更新数据...")
+		u := NewUpdateKline(Codes, filepath.Join(DatabaseDir, "kline"), Coroutines)
+		err = u.Run(context.Background(), m)
+		logs.PrintErr(err)
+		logs.Info("导出数据...")
+		e := NewExportKline(
+			Codes,
+			[]int{time.Now().Year()},
+			filepath.Join(DatabaseDir, "kline"),
+			filepath.Join(ExportDir),
+		)
+		err = e.Run(context.Background(), m)
+		logs.PrintErr(err)
+		logs.Info("任务完成...")
+	}
+
+	corn := cron.New(cron.WithSeconds())
+	corn.AddFunc(Spec, f)
+
+	if Startup {
+		f()
+	}
+
+	corn.Run()
+}
