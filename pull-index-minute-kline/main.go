26d6c327a1fe54dc94aeb5df81c45ac8aa3227e5
injoyai <1113655791@qq.com>
2025-10-17 16:41:40 +0800

    101

--- pull-index-minute-kline/main.go
+++ pull-index-minute-kline/main.go
@@ -1,6 +1,15 @@
 package main
 
+import (
+	"github.com/injoyai/conv"
+	"github.com/injoyai/goutil/database/sqlite"
+	"github.com/injoyai/tdx"
+	"path/filepath"
+	"time"
+)
+
 var (
+	Dir   = "./data/database"
 	Codes = []string{
 		"sh000001",
 		"sz399001",
@@ -10,4 +19,22 @@ var (
 
 func main() {
 
+	for _, code := range Codes {
+
+	}
+
+}
+
+func update(c *tdx.Client, code string) error {
+	dir := filepath.Join(Dir, conv.String(time.Now().Year()))
+	filename := filepath.Join(dir, code+".db")
+	db, err := sqlite.NewXorm(filename)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	db.Desc("")
+
+	return nil
 }
