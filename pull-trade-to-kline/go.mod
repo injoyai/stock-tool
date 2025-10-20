d7b7ff41edac27e7d0a29e87bb34a9ee440131b9
injoyai <1113655791@qq.com>
2025-10-20 11:20:53 +0800

    101

--- pull-trade-to-kline/go.mod
+++ pull-trade-to-kline/go.mod
@@ -1,12 +1,15 @@
 module pull-trade-to-kline
 
-go 1.22.0
+go 1.25.0
 
 require (
 	github.com/injoyai/base v1.2.17
+	github.com/injoyai/conv v1.2.5
 	github.com/injoyai/goutil v1.2.20
 	github.com/injoyai/logs v1.0.12
-	github.com/injoyai/tdx v0.0.32
+	github.com/injoyai/tdx v0.0.39
+	github.com/robfig/cron/v3 v3.0.1
+	xorm.io/xorm v1.3.9
 )
 
 require (
@@ -15,11 +18,11 @@ require (
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/fatih/color v1.18.0 // indirect
 	github.com/glebarez/go-sqlite v1.22.0 // indirect
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/goccy/go-json v0.8.1 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/grafov/m3u8 v0.12.1 // indirect
-	github.com/injoyai/conv v1.2.5 // indirect
 	github.com/injoyai/ios v1.2.2 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/kr/text v0.2.0 // indirect
@@ -29,7 +32,7 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.1 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
-	github.com/robfig/cron/v3 v3.0.1 // indirect
+	github.com/satori/go.uuid v1.2.0 // indirect
 	github.com/syndtr/goleveldb v1.0.0 // indirect
 	golang.org/x/mod v0.21.0 // indirect
 	golang.org/x/net v0.30.0 // indirect
@@ -43,5 +46,4 @@ require (
 	modernc.org/sqlite v1.28.0 // indirect
 	xorm.io/builder v0.3.11-0.20220531020008-1bd24a7dc978 // indirect
 	xorm.io/core v0.7.3 // indirect
-	xorm.io/xorm v1.3.9 // indirect
 )
--- pull-trade-to-kline/main.go
+++ pull-trade-to-kline/main.go
@@ -2,141 +2,252 @@ package main
 
 import (
 	"github.com/injoyai/base/chans"
+	"github.com/injoyai/conv"
+	"github.com/injoyai/goutil/database/sqlite"
+	"github.com/injoyai/goutil/database/xorms"
+	"github.com/injoyai/goutil/g"
 	"github.com/injoyai/goutil/oss"
 	"github.com/injoyai/goutil/other/csv"
 	"github.com/injoyai/goutil/str/bar/v2"
 	"github.com/injoyai/logs"
 	"github.com/injoyai/tdx"
 	"github.com/injoyai/tdx/protocol"
-	"os"
 	"path/filepath"
 	"time"
+	"xorm.io/xorm"
 )
 
 var (
+	DatabaseDir   = "./data/database/kline"
+	CsvDir        = "./data/csv"
+	Clients       = 3
+	Goroutine     = 3
+	Startup       = true
+	Retry         = 3
+	RetryInterval = time.Second
+	Indexes       = []string{
+		"sh000001", //上证指数
+		"sz399001", //深证成指
+		"sz399006", //创业板指
+		"sh000016", //上证50
+		"sh000688", //科创50
+		"sh000010", //上证180
+		"sh000300", //沪深300
+		"sh000905", //中证500
+		"sh000852", //中证1000
+		"sh000932", //中证消费指数,
+		"sh000827", //中证环保指数,
+	}
+	indexesMap = func() map[string]bool {
+		m := make(map[string]bool)
+		for _, v := range Indexes {
+			m[v] = true
+		}
+		return m
+	}()
 	Codes = []string{
-		//"sz000001",
+		//"sh600000",
 	}
-	Indexes = []string{
-		"sh000001",
-		"sz399001",
-		"sz399006",
-	}
-	Dir     = "./data"
-	EndDate = time.Date(2024, 12, 31, 15, 0, 0, 0, time.Local)
+	Start  = time.Date(2000, 1, 1, 0, 0, 0, 0, time.Local)
+	End    = time.Now().AddDate(0, -4, 0)
+	CsvEnd = time.Date(2025, 1, 1, 0, 0, 0, 0, time.Local)
 )
 
 func main() {
-	m, err := tdx.NewManage(&tdx.ManageConfig{Number: 3})
+	m, err := tdx.NewManage(&tdx.ManageConfig{Number: Clients})
 	logs.PanicErr(err)
 
-	cs, err := tdx.GetBjCodes()
-	logs.PanicErr(err)
+	Codes = append(Codes, Indexes...)
 
-	for _, v := range cs {
-		Codes = append(Codes, "bj"+v.Code)
-	}
-
-	PullIndexes(m, Indexes, Dir)
-
-	PullStocks(m, Codes, Dir)
-
-	//select {}
+	logs.PrintErr(pull(m, Codes))
 }
 
-func PullStocks(m *tdx.Manage, codes []string, dir string) {
-	dir = filepath.Join(dir, "股票")
-	os.MkdirAll(dir, os.ModePerm)
-	if len(codes) == 0 {
-		codes = m.Codes.GetStocks()
-	}
-	if len(codes) == 0 {
-		return
-	}
+func pull(m *tdx.Manage, codes []string) error {
 	b := bar.New(
 		bar.WithTotal(int64(len(codes))),
-		bar.WithPrefix("["+codes[0]+"]"),
+		bar.WithPrefix("xx000000"),
 		bar.WithFlush(),
 	)
 	defer b.Close()
-	wg := chans.NewWaitLimit(10)
+	wg := chans.NewWaitLimit(Goroutine)
 	for i, _ := range codes {
-		code := codes[i]
 		wg.Add()
-		m.Go(func(c *tdx.Client) {
-			b.SetPrefix("[" + code + "]")
-			b.Flush()
+		go func(code string) {
 			defer func() {
 				b.Add(1)
 				b.Flush()
 				wg.Done()
 			}()
-			ls, err := pullTrades(c, m.Workday, code)
-			if err != nil {
-				logs.Err(err)
-				return
-			}
-			ks1 := ls.Klines()
-			save(ks1, dir, code, "1分钟")
-			save(ks1.Merge(5), dir, code, "5分钟")
-			save(ks1.Merge(15), dir, code, "15分钟")
-			save(ks1.Merge(30), dir, code, "30分钟")
-			save(ks1.Merge(60), dir, code, "60分钟")
-		})
-	}
-	wg.Wait()
-}
-
-// PullIndexes 拉取指数
-func PullIndexes(m *tdx.Manage, indexes []string, dir string) {
-	if len(indexes) == 0 {
-		return
-	}
-	dir = filepath.Join(dir, "指数")
-	os.MkdirAll(dir, os.ModePerm)
-	b := bar.New(
-		bar.WithTotal(int64(len(indexes))),
-		bar.WithPrefix("["+indexes[0]+"]"),
-		bar.WithFlush(),
-	)
-	defer b.Close()
-	wg := chans.NewWaitLimit(10)
-	for i, _ := range indexes {
-		code := indexes[i]
-		wg.Add()
-		m.Go(func(c *tdx.Client) {
 			b.SetPrefix("[" + code + "]")
 			b.Flush()
-			defer func() {
-				b.Add(1)
-				b.Flush()
-				wg.Done()
-			}()
-			ls, err := pullTrades(c, m.Workday, code)
+			var (
+				ts  protocol.Trades
+				err error
+			)
+			err = g.Retry(func() error {
+				return m.Do(func(c *tdx.Client) error {
+					ts, err = pullOne(c, m.Workday, code)
+					return err
+				})
+			}, Retry, RetryInterval)
 			if err != nil {
-				logs.Err(err)
+				b.Logf("[错误] [%s] %s", code, err)
+				b.Flush()
 				return
 			}
-			ks1 := ls.Klines()
-			for ii, v := range ks1 {
-				ks1[ii].Amount = protocol.Price(v.Volume * 100 * 1000)
-				ks1[ii].Volume = 0
+			err = save(ts.Klines(), code)
+			if err != nil {
+				b.Logf("[错误] [%s] %s", code, err)
+				b.Flush()
+				return
 			}
-			save(ks1, dir, code, "1分钟")
-			save(ks1.Merge(5), dir, code, "5分钟")
-			save(ks1.Merge(15), dir, code, "15分钟")
-			save(ks1.Merge(30), dir, code, "30分钟")
-			save(ks1.Merge(60), dir, code, "60分钟")
-		})
+		}(codes[i])
 	}
 	wg.Wait()
+	return nil
 }
 
-func save(ks protocol.Klines, dir, code, typeName string) error {
+func pullOne(c *tdx.Client, w *tdx.Workday, code string) (ts protocol.Trades, err error) {
+	resp, err := c.GetKlineMonthAll(code)
+	if err != nil {
+		return nil, err
+	}
+	if len(resp.List) == 0 {
+		return nil, nil
+	}
+	start := time.Date(resp.List[0].Time.Year(), resp.List[0].Time.Month(), 1, 0, 0, 0, 0, resp.List[0].Time.Location())
+	var res *protocol.TradeResp
+	w.Range(start, End, func(t time.Time) bool {
+		res, err = c.GetHistoryTradeDay(t.Format("20060102"), code)
+		if err != nil {
+			return false
+		}
+		ts = append(ts, res.List...)
+		return true
+	})
+	return
+}
+
+func save(ks protocol.Klines, code string) error {
+	//按年分割
+	m := map[int]protocol.Klines{}
+	for i := range ks {
+		if indexesMap[code] {
+			ks[i].Amount = protocol.Price(ks[i].Volume * 100 * 1000)
+			ks[i].Volume = 0
+		}
+		m[ks[i].Time.Year()] = append(m[ks[i].Time.Year()], ks[i])
+	}
+	for year, ls := range m {
+
+		k1 := toModel(ls)
+		k5 := toModel(ls.Merge(5))
+		k15 := toModel(ls.Merge(15))
+		k30 := toModel(ls.Merge(30))
+		k60 := toModel(ls.Merge(60))
+
+		err := insertDB(year, code, k1, k5, k15, k30, k60)
+		if err != nil {
+			return err
+		}
+	}
+	return exportCsv(ks, code)
+}
+
+func toModel(ks protocol.Klines) []any {
+	inserts := make([]any, len(ks))
+	for i, v := range ks {
+		inserts[i] = &KlineBase{
+			Date:   v.Time.Unix(),
+			Year:   v.Time.Year(),
+			Month:  int(v.Time.Month()),
+			Day:    v.Time.Day(),
+			Hour:   v.Time.Hour(),
+			Minute: v.Time.Minute(),
+			Open:   v.Open.Float64(),
+			High:   v.High.Float64(),
+			Low:    v.Low.Float64(),
+			Close:  v.Close.Float64(),
+			Volume: int(v.Volume),
+			Amount: v.Amount.Float64(),
+		}
+	}
+	return inserts
+}
+
+func insertDB(year int, code string, k1, k5, k15, k30, k60 []any) error {
+	if len(k1) == 0 {
+		return nil
+	}
+	filename := filepath.Join(DatabaseDir, conv.String(year), code+".db")
+	db, err := sqlite.NewXorm(filename)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+	if err = db.Sync2(new(KlineMinute1), new(KlineMinute5), new(KlineMinute15), new(KlineMinute30), new(KlineMinute60)); err != nil {
+		return err
+	}
+	if err = _insertDB(db, new(KlineMinute1), k1); err != nil {
+		return err
+	}
+	if err = _insertDB(db, new(KlineMinute5), k5); err != nil {
+		return err
+	}
+	if err = _insertDB(db, new(KlineMinute15), k15); err != nil {
+		return err
+	}
+	if err = _insertDB(db, new(KlineMinute30), k30); err != nil {
+		return err
+	}
+	if err = _insertDB(db, new(KlineMinute60), k60); err != nil {
+		return err
+	}
+	return nil
+}
+
+func _insertDB(db *xorms.Engine, table Timer, inserts []any) error {
+	return db.SessionFunc(func(session *xorm.Session) error {
+		if _, err := session.Where("ID>0").Delete(table); err != nil {
+			return err
+		}
+		_, err := session.Table(table).Insert(inserts...)
+		return err
+	})
+}
+
+func exportCsv(ks protocol.Klines, code string) error {
+	err := _exportCsv(ks, code, "1分钟")
+	if err != nil {
+		return err
+	}
+	err = _exportCsv(ks.Merge(5), code, "5分钟")
+	if err != nil {
+		return err
+	}
+	err = _exportCsv(ks.Merge(15), code, "15分钟")
+	if err != nil {
+		return err
+	}
+	err = _exportCsv(ks.Merge(30), code, "30分钟")
+	if err != nil {
+		return err
+	}
+	err = _exportCsv(ks.Merge(60), code, "60分钟")
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func _exportCsv(ks protocol.Klines, code, _type string) error {
 	data := [][]any{
 		{"日期", "时间", "开盘", "最高", "最低", "收盘", "成交量", "成交额"},
 	}
 	for _, v := range ks {
+		if v.Time.After(CsvEnd) {
+			continue
+		}
 		data = append(data, []any{
 			v.Time.Format(time.DateOnly),
 			v.Time.Format("15:04"),
@@ -152,29 +263,6 @@ func save(ks protocol.Klines, dir, code, typeName string) error {
 	if err != nil {
 		return err
 	}
-	filename := filepath.Join(dir, typeName, code+"-"+typeName+".csv")
+	filename := filepath.Join(CsvDir, _type, code+".csv")
 	return oss.New(filename, buf)
-}
-
-func pullTrades(c *tdx.Client, w *tdx.Workday, code string) (protocol.Trades, error) {
-	ls := protocol.Trades(nil)
-	resp, err := c.GetKlineMonthAll(code)
-	if err != nil {
-		return nil, err
-	}
-	if len(resp.List) == 0 {
-		return nil, nil
-	}
-	start := time.Date(resp.List[0].Time.Year(), resp.List[0].Time.Month(), 1, 0, 0, 0, 0, resp.List[0].Time.Location())
-	var res *protocol.TradeResp
-	w.Range(start, EndDate, func(t time.Time) bool {
-		res, err = c.GetHistoryTradeDay(t.Format("20060102"), code)
-		if err != nil {
-			return false
-		}
-		ls = append(ls, res.List...)
-		return true
-	})
-
-	return ls, nil
 }
