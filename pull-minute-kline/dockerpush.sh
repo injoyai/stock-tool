dbdc0f32e0f68b8a5de814287a548147f8723da8
injoyai <1113655791@qq.com>
2025-10-28 08:54:07 +0800

    101

--- pull-minute-kline/dockerpush.sh
+++ pull-minute-kline/dockerpush.sh
@@ -3,6 +3,6 @@ name="pull-minute-kline"
 GOOS=linux GOARCH=amd64 go build -v -ldflags="-w -s" -o ./$name
 
 docker pull --platform=linux/amd64 alpine:latest
-docker build --platform=linux/amd64 --push -t docker.io/injoyai/pull-minute-kline-amd64:latest -f ./Dockerfile .
+docker build --platform=linux/amd64 --push -t docker.io/injoyai/$name-amd64:latest -f ./Dockerfile .
 
 sleep 8
