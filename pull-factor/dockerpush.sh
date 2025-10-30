dccf78ebe87e5e39ccd495240ce01e7646aafa99
injoyai <1113655791@qq.com>
2025-10-30 08:53:54 +0800

    101

--- pull-factor/dockerpush.sh
+++ pull-factor/dockerpush.sh
@@ -1,6 +1,6 @@
 name="pull-factor"
 
-GOOS=linux GOARCH=amd64 go build -v -ldflags="-w -s" -o ./$name
+GOOS=linux GOARCH=amd64 go build -v -ldflags="-w -s" -o ./bin/$name
 
 docker pull --platform=linux/amd64 alpine:latest
 docker build --platform=linux/amd64 --push -t docker.io/injoyai/$name-amd64:latest -f ./Dockerfile .
