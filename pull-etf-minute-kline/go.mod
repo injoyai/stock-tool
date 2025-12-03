8307c1207dd07fcf4da310b99938b6b2981c6b84
钱纯净 <1113655791@qq.com>
2025-12-03 23:10:08 +0800

    101

--- pull-etf-minute-kline/go.mod
+++ pull-etf-minute-kline/go.mod
@@ -2,7 +2,13 @@ module pull-etf-minute-kline
 
 go 1.25.0
 
-require github.com/injoyai/tdx v0.0.57
+require (
+	github.com/injoyai/bar v0.0.8
+	github.com/injoyai/conv v1.2.5
+	github.com/injoyai/logs v1.0.12
+	github.com/injoyai/tdx v0.0.57
+	github.com/robfig/cron/v3 v3.0.1
+)
 
 require (
 	github.com/dustin/go-humanize v1.0.1 // indirect
@@ -12,10 +18,9 @@ require (
 	github.com/goccy/go-json v0.8.1 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/uuid v1.5.0 // indirect
+	github.com/grafov/m3u8 v0.12.1 // indirect
 	github.com/injoyai/base v1.2.17 // indirect
-	github.com/injoyai/conv v1.2.5 // indirect
 	github.com/injoyai/ios v1.2.2 // indirect
-	github.com/injoyai/logs v1.0.12 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -24,8 +29,6 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.1 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
-	github.com/robfig/cron/v3 v3.0.1 // indirect
-	github.com/rogpeppe/go-internal v1.14.1 // indirect
 	github.com/syndtr/goleveldb v1.0.0 // indirect
 	golang.org/x/net v0.30.0 // indirect
 	golang.org/x/sys v0.26.0 // indirect
--- pull-etf-minute-kline/main.go
+++ pull-etf-minute-kline/main.go
@@ -1,24 +1,75 @@
 package main
 
 import (
+	"github.com/injoyai/bar"
 	"github.com/injoyai/conv/cfg"
 	"github.com/injoyai/logs"
-	"github.com/injoyai/tdx/extend"
+	"github.com/injoyai/tdx"
 )
 
 var (
-	address = cfg.GetString("address", "http://127.0.0.1:20000")
+	clients    = cfg.GetInt("clients", 3)
+	coroutines = cfg.GetInt("coroutines", 10)
+	retry      = cfg.GetInt("retry", 3)
+	address    = cfg.GetString("address", "http://127.0.0.1:20000")
+	spec       = cfg.GetString("spec", "0 15 15 * * *")
 )
 
 func main() {
-	cs, err := extend.DialCodesHTTP(address)
+
+	m, err := tdx.NewManage(
+		tdx.WithClients(clients),
+		tdx.WithDialCodes(func(c *tdx.Client, database string) (tdx.ICodes, error) {
+			return DialCodesHTTP(address)
+		}),
+	)
 	logs.PanicErr(err)
 
-	codes := cs.GetETFCodes()
-	for _, v := range codes {
-		logs.Debug(v)
+	//更新
+	logs.PanicErr(Update(m))
+
+	m.AddWorkdayTask(spec, func(m *tdx.Manage) {
+		logs.PrintErr(Update(m))
+	})
+
+	select {}
+}
+
+func Update(m *tdx.Manage) error {
+	codes := m.Codes.GetETFCodes()
+
+	b := bar.NewCoroutine(len(codes), coroutines)
+	defer b.Close()
+
+	for i := range codes {
+		code := codes[i]
+		b.GoRetry(func() error {
+			return m.Do(func(c *tdx.Client) error { return update(c, code) })
+		}, retry)
 	}
 
-	logs.Debug("总数:", len(codes))
+	return nil
+}
 
+func update(c *tdx.Client, code string) error {
+
+	//读取数据库最后一条数据
+
+	//拉取数据
+
+	//更新到数据库
+
+	return nil
+}
+
+// exportThisYear 导出今年数据
+func exportThisYear() error {
+
+	//打开数据库
+
+	//读取今年数据
+
+	//导出
+
+	return nil
 }
