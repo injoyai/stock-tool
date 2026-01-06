e00e23be19c27500badc8e416bdaadbcec08d6a7
钱纯净 <1113655791@qq.com>
2026-01-06 17:24:19 +0800

    101

--- pull-minute241-kline/dockerpush.sh
+++ pull-minute241-kline/dockerpush.sh
@@ -3,6 +3,6 @@ name="pull-stock-minute-kline"
 GOOS=linux GOARCH=amd64 go build -v -ldflags="-w -s" -o ./$name
 
 docker pull --platform=linux/amd64 alpine:latest
-docker build --platform=linux/amd64 --push -t crpi-ayrx20sj8nkmrgmh.cn-hangzhou.personal.cr.aliyuncs.com/injoyai/$name-amd64:latest -f ./Dockerfile .
+docker build --platform=linux/amd64 --push -t crpi-ayrx20sj8nkmrgmh.cn-hangzhou.personal.cr.aliyuncs.com/injoyai/$name:latest -f ./Dockerfile .
 
 sleep 8
