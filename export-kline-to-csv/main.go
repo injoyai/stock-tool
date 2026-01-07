a8526d06f091d3eb9c4ff7c44d8d66ad246d78f0
钱纯净 <1113655791@qq.com>
2026-01-07 17:46:31 +0800

    101

---
+++ export-kline-to-csv/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+	"runtime/debug"
+
+	"github.com/injoyai/bar"
+	"github.com/injoyai/goutil/database/sqlite"
+	"github.com/injoyai/goutil/oss"
+	"github.com/injoyai/goutil/other/csv"
+	"github.com/injoyai/logs"
+	"github.com/injoyai/tdx"
+	"github.com/injoyai/tdx/protocol"
+)
+
+const (
+	DatabaseDir = "./data/database/kline"
+	ExportDir   = "./data/export/csv"
+	Coroutines  = 10
+)
+
+func main() {
+
+	gb, err := tdx.NewGbbq()
+	logs.PanicErr(err)
+
+	es, err := os.ReadDir(DatabaseDir)
+	logs.PanicErr(err)
+
+	b := bar.NewCoroutine(len(es), Coroutines, bar.WithPrefix("[xx000000]"))
+	defer b.Close()
+
+	for _, v := range es {
+		dir := filepath.Join(DatabaseDir, v.Name())
+		b.SetPrefix("[" + v.Name() + "]")
+		b.GoRetry(func() error { return export(gb, dir, ExportDir) }, tdx.DefaultRetry)
+	}
+
+	b.Wait()
+
+	logs.Info("完成...")
+
+}
+
+func export(gb *tdx.Gbbq, databaseDir, exportDir string) error {
+
+	defer func() {
+		if e := recover(); e != nil {
+			debug.PrintStack()
+		}
+	}()
+
+	code := filepath.Base(databaseDir)
+	kss := protocol.Klines{}
+	err := oss.RangeFileInfo(databaseDir, func(info *oss.FileInfo) (bool, error) {
+		ks, err := loading(info.FullName())
+		if err != nil {
+			return false, err
+		}
+		kss = append(kss, ks...)
+		return true, nil
+	})
+	if err != nil {
+		return err
+	}
+	kss.Sort()
+
+	data := [][]any{
+		{"代码", "日期", "开盘", "最高", "最低", "收盘", "成交量(股)", "成交额(元)", "涨跌", "涨跌幅(%)", "换手率(%)", "流通股本(股)", "总股本(股)"},
+	}
+
+	var last *protocol.Equity
+	for _, v := range kss {
+		eq := gb.GetEquity(code, v.Time)
+		if eq == nil {
+			eq = last
+			if eq == nil {
+				continue
+			}
+		}
+		last = eq
+		data = append(data, []any{
+			code, v.Time.Format("2006-01-02 15:04:05"),
+			v.Open.Float64(), v.High.Float64(), v.Low.Float64(), v.Close.Float64(),
+			v.Volume * 100, v.Amount.Float64(),
+			v.RisePrice().Float64(), v.RiseRate(),
+			gb.GetTurnover(code, v.Time, v.Volume*100),
+			int64(eq.Float), int64(eq.Total),
+		})
+	}
+
+	buf, err := csv.Export(data)
+	if err != nil {
+		return err
+	}
+
+	filename := filepath.Join(exportDir, code+".csv")
+	return oss.New(filename, buf)
+}
+
+func loading(filename string) (protocol.Klines, error) {
+	db, err := sqlite.NewXorm(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+	ks := protocol.Klines{}
+	err = db.Find(&ks)
+	return ks, err
+}
