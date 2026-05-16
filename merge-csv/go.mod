80197b5e0d220d822fd8e1f2f69f8d1d657eeecd
钱纯净 <1113655791@qq.com>
2026-05-16 13:16:12 +0800

    101

--- merge-csv/go.mod
+++ merge-csv/go.mod
@@ -1,15 +1,17 @@
 module merge-csv
 
-go 1.23
+go 1.20
 
 require (
+	github.com/injoyai/bar v0.0.11
 	github.com/injoyai/goutil v1.2.26
 	github.com/injoyai/logs v1.0.12
 )
 
 require (
 	github.com/fatih/color v1.18.0 // indirect
-	github.com/injoyai/base v1.2.13 // indirect
+	github.com/grafov/m3u8 v0.12.1 // indirect
+	github.com/injoyai/base v1.2.20 // indirect
 	github.com/injoyai/conv v1.2.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/kr/text v0.2.0 // indirect
@@ -19,7 +21,9 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.1 // indirect
 	github.com/satori/go.uuid v1.2.0 // indirect
+	golang.org/x/net v0.30.0 // indirect
 	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- merge-csv/main.go
+++ merge-csv/main.go
@@ -4,75 +4,127 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"sort"
 	"strings"
 
+	"github.com/injoyai/bar"
 	"github.com/injoyai/goutil/g"
-	"github.com/injoyai/goutil/oss"
 	"github.com/injoyai/goutil/other/csv"
+	"github.com/injoyai/goutil/str/regexps"
 	"github.com/injoyai/logs"
 )
 
 func init() {
-	logs.SetFormatter(logs.TimeFormatter)
+	//logs.SetFormatter(logs.TimeFormatter)
 }
 
 func main() {
 
 	defer func() { g.Input("按回车键退出...") }()
 
-	dirHistory := "./2000-2025"
-	dirThisyear := "./2026"
-	dir3 := "./2000-2026"
+	es, err := os.ReadDir("./")
+	logs.PanicErr(err)
 
-	if !oss.Exists(dirHistory) {
-		logs.Err("文件夹[2000-2025]不存在")
-		return
-	}
-
-	if !oss.Exists(dirThisyear) {
-		logs.Err("文件夹[2026]不存在")
-		return
-	}
-
-	err := os.MkdirAll(dir3, 0777)
-	if err != nil {
-		logs.Err(err)
-		return
-	}
-
-	oss.RangeFile(dirHistory, func(info *oss.FileInfo, f *os.File) (bool, error) {
-
-		logs.Info(info.Name())
-
-		filename2025 := filepath.Join(dirThisyear, info.Name())
-
-		output := filepath.Join(dir3, info.Name())
-		ff, err := os.Create(output)
-		if err != nil {
-			logs.Err(err)
-			return true, nil
+	dirs := make([]string, 0)
+	for _, e := range es {
+		if len(e.Name()) >= 4 && regexps.Is("^[0-9]{4}.*?", e.Name()) {
+			dirs = append(dirs, e.Name())
 		}
-		defer ff.Close()
+	}
 
-		bs, err := io.ReadAll(f)
-		if err != nil {
-			logs.Err(err)
-			return true, nil
-		}
-
-		ff.Write(bs)
-
-		csv.ImportRange(filename2025, func(i int, line []string) bool {
-			if i == 0 {
-				i++
-				return true
-			}
-			ff.Write([]byte(strings.Join(line, ",") + "\n"))
-			return true
-		})
-
-		return true, nil
-
+	sort.Slice(dirs, func(i, j int) bool {
+		return dirs[i] < dirs[j]
 	})
 
+	if len(dirs) <= 1 {
+		logs.Err("文件夹不足")
+		return
+	}
+
+	outputDir := dirs[0][:4] + "-" + dirs[len(dirs)-1][:4]
+
+	err = do(dirs, outputDir, "1分钟")
+	logs.PrintErr(err)
+
+	err = do(dirs, outputDir, "5分钟")
+	logs.PrintErr(err)
+
+}
+
+func do(dirs []string, outputDir, _type string) error {
+	historyDir := filepath.Join(dirs[0], _type)
+
+	es, err := os.ReadDir(historyDir)
+	if err != nil {
+		return err
+	}
+
+	b := bar.New(
+		bar.WithTotal(int64(len(es))),
+		bar.WithPrefix("[xx000000]"),
+		bar.WithFlush(),
+	)
+	defer b.Close()
+
+	for _, e := range es {
+
+		err := func() error {
+			b.SetPrefix("[" + e.Name() + "]")
+			b.Flush()
+
+			defer func() {
+				b.Add(1)
+				b.Flush()
+			}()
+
+			err = os.MkdirAll(filepath.Join(outputDir, _type), 0777)
+			if err != nil {
+				return err
+			}
+			outputFilename := filepath.Join(outputDir, _type, e.Name())
+			out, err := os.Create(outputFilename)
+			if err != nil {
+				logs.Err(err)
+				return err
+			}
+			defer out.Close()
+
+			historyFilename := filepath.Join(historyDir, e.Name())
+			historyFile, err := os.Open(historyFilename)
+			if err != nil {
+				return err
+			}
+			defer historyFile.Close()
+
+			if _, err = io.Copy(out, historyFile); err != nil {
+				return err
+			}
+
+			for _, dir := range dirs[1:] {
+				if dir == outputDir {
+					continue
+				}
+				filename := filepath.Join(dir, _type, e.Name())
+				err = csv.ImportRange(filename, func(i int, line []string) bool {
+					if i == 0 {
+						i++
+						return true
+					}
+					out.Write([]byte(strings.Join(line, ",") + "\n"))
+					return true
+				})
+				if err != nil {
+					return err
+				}
+			}
+			return nil
+		}()
+		if err != nil {
+			b.Logf("[错误][%s] %v", e, err)
+			b.Flush()
+			continue
+		}
+	}
+
+	return nil
 }
